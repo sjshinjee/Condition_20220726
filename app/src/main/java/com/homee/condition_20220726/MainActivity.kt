180abf5e595dbbe4da5153a0073cfbe135632bf4
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-12 15:28:34 +0900

    버튼3개 조건문 Log검사만

--- app/src/main/java/com/homee/condition_20220726/MainActivity.kt
+++ app/src/main/java/com/homee/condition_20220726/MainActivity.kt
@@ -2,37 +2,29 @@ package com.homee.condition_20220726
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-//            val age =  inputEdt.text.toString().toInt()
-//
-//            if (age < 10){
-//                answerTxt.text = "아이입니다"
-//            }
-//            else if(age < 20){
-//                answerTxt.text = "청소년입니다"
-//            }
-//            else{
-//                answerTxt.text = "성인입니다"
-//            }
-
-            val inputData : String = inputEdt.text.toString()
-
-            if (inputData == "김민철"){
-                answerTxt.text = "김민철이 맞습니다"
-            }
-            else {
-                answerTxt.text = "김민철이 아닙니다"
+            variationTestBtn.setOnClickListener {
+                val myAge = 10
+                if(myAge<10){
+                    Log.d("나이검사","유아입니다")
+                }
+                else if(myAge<20){
+                    Log.d("나이검사","청소년입니다")
+                }
+                else{
+                    Log.d("나이검사","성인입니다")
+                }
             }
 
 
-        }
 
 
     }
