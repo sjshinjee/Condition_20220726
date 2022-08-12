e4db34c7ee618f550e29660bf909551d1802ffe1
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-12 15:50:03 +0900

    취업여부 논리연산자 이용해서 Log 돌려봄

--- app/src/main/java/com/homee/condition_20220726/MainActivity.kt
+++ app/src/main/java/com/homee/condition_20220726/MainActivity.kt
@@ -12,27 +12,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            variationTestBtn.setOnClickListener {
-//                val myAge = 10
-//                if(myAge<10){
-//                    Log.d("나이검사","유아입니다")
-//                }
-//                else if(myAge<20){
-//                    Log.d("나이검사","청소년입니다")
-//                }
-//                else{
-//                    Log.d("나이검사","성인입니다")
-//                }
+        conditionBtn2.setOnClickListener {
+            //장기근속 1연봉 2야근유뮤 3거리
+            val salary = 3800
+            val dayNight = true
+            val distance = 4
 
-                val birthYear = 1997
-                val myAge = 2022-birthYear+1
-
-                if(myAge>15){
-                    Log.d("나이검사",myAge.toString())
-                    Toast.makeText(this, "유아가 아님", Toast.LENGTH_SHORT).show()
-                }
+            //연봉이 4000이상이거나 거리가 5분 이내거나
+            if(salary>4000 || distance<=5){
+                    Log.d("입사 여부","취업")
             }
 
+            //연봉이 3000이상이거나 야근이 없거나
+            if(salary>3000 || !dayNight){  //dayNight!=true와 같은 문장
+                    Log.d("입사 여부", "취업")
+            }
+
+            //야근이 없어야하고 거리고 10분 이내
+            if(dayNight!=true && distance<10){
+                    Log.d("입사 여부", "취업")
+            }
+
+
+
+        }
 
 
 
